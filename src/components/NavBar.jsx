beffab5e9c5eb98bc7b06cf86396da1c2f97023f
Paul Gerritsen <paul188297@gmail.com>
2023-11-16 16:13:10 -0700

    styled the homepage and Navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,8 +2,9 @@ import React from 'react'
 import axios from 'axios'
 import { useDispatch, useSelector } from 'react-redux'
 import { useEffect } from 'react'
-import { Container, Navbar, Nav, NavLink, NavDropdown } from 'react-bootstrap'
+import { Container, Navbar, Nav, NavLink, NavDropdown, Row, Col } from 'react-bootstrap'
 import { useNavigate } from 'react-router-dom'
+import { GiMountainClimbing } from "react-icons/gi";
 
 const NavBar = () => {
   const dispatch = useDispatch()
@@ -30,44 +31,56 @@ const NavBar = () => {
   useEffect(() => sessionCheck, [userId])
 
   return (
-    <div>
       <Navbar>
-        <Container fluid>
-          <Navbar.Brand href='/'>BRAND</Navbar.Brand>
-          <Navbar.Toggle/>
-          <Navbar.Collapse>
-            <NavDropdown title='Climbs'>
-              <NavDropdown.Item onClick={() => {dispatch({type: 'filter', payload: 'All'}); navigate('/climbs')}}>All Climbs</NavDropdown.Item>
-              <NavDropdown.Item onClick={() => {dispatch({type: 'filter', payload: 'Boulder'}); navigate('/climbs')}} >Boulders</NavDropdown.Item>
-              <NavDropdown.Item onClick={() => {dispatch({type: 'filter', payload: 'Route'}); navigate('/climbs')}} >Routes</NavDropdown.Item>
-              {userId &&
-              <>
-                <NavDropdown.Divider/>
-                <NavDropdown.Item onClick={() => {dispatch({type: 'filter', payload: 'Private'}); navigate('/climbs')}}>My climbs</NavDropdown.Item>
-              </>
-              }
-            </NavDropdown>
-            <Nav>
-              <NavLink href='/shop'>
-                Shop
-              </NavLink>
-            </Nav>
-            <Nav className='justify-content-end'>
-              {userId &&
-              <NavLink href={`/profile/${userId}`}>
-                {username}
-              </NavLink>
-              }
-              {!userId &&
-              <NavLink href='/login'>
-                Log in
-              </NavLink>
-              }
-            </Nav>
-          </Navbar.Collapse>
+        <Container fluid >
+          <Row className='min-vw-100'> 
+            <Col xs={{span: 1}}>
+              <Navbar.Brand href='/'>
+                <GiMountainClimbing size={40}/>
+              </Navbar.Brand>
+            </Col>
+            <Navbar.Toggle/>
+            <Col xs={{span: 11}}>
+              <Navbar.Collapse>
+                <Col xs={{span: 1}} className='fs-4'>
+                  <NavDropdown title='Climbs'>
+                    <NavDropdown.Item onClick={() => {dispatch({type: 'filter', payload: 'All'}); navigate('/climbs')}}>All Climbs</NavDropdown.Item>
+                    <NavDropdown.Item onClick={() => {dispatch({type: 'filter', payload: 'Boulder'}); navigate('/climbs')}} >Boulders</NavDropdown.Item>
+                    <NavDropdown.Item onClick={() => {dispatch({type: 'filter', payload: 'Route'}); navigate('/climbs')}} >Routes</NavDropdown.Item>
+                    {userId &&
+                    <>
+                      <NavDropdown.Divider/>
+                      <NavDropdown.Item onClick={() => {dispatch({type: 'filter', payload: 'Private'}); navigate('/climbs')}}>My climbs</NavDropdown.Item>
+                    </>
+                    }
+                  </NavDropdown>
+                </Col>
+                <Col xs={{span: 1}} className='fs-4'>
+                  <Nav > 
+                    <NavLink href='/shop'>
+                      Shop
+                    </NavLink>
+                  </Nav>
+                </Col>
+                <Col xs={{span: 2, offset: 8}} className='fs-4'>
+                  <Nav >
+                    {userId &&
+                    <NavLink href={`/profile/${userId}`} >
+                      {username}
+                    </NavLink>
+                    }
+                    {!userId &&
+                    <NavLink href='/login'>
+                      Log in
+                    </NavLink>
+                    }
+                  </Nav>
+                </Col>
+                </Navbar.Collapse>
+            </Col>
+            </Row>
         </Container>
       </Navbar>
-    </div>
   )
 }
 
--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -3,7 +3,7 @@ import ClimbCard from '../components/ClimbCard'
 import ShopCard from '../components/ShopCard'
 import { useLoaderData } from 'react-router-dom'
 import lodash from 'lodash'
-import { Container, Row } from 'react-bootstrap'
+import { Col, Container, Row } from 'react-bootstrap'
 
 const Homepage = () => {
 
@@ -11,23 +11,29 @@ const Homepage = () => {
 
   const publicClimbs = climbs.filter((climb) => climb.isPublic)
 
-  const randomClimbs = lodash.sampleSize(publicClimbs, 2)
+  const randomClimbs = lodash.sampleSize(publicClimbs, 4)
 
   const randomClimbCards = randomClimbs.map((climb) => (
     <ClimbCard key={climb.climbId} climb={climb}/>
   ))
 
-  const randomItems = lodash.sampleSize(shop, 2)
+  const randomItems = lodash.sampleSize(shop, 3)
 
   const randomItemCards = randomItems.map((item) => (
     <ShopCard key={item.itemId} item={item} />
   ))
   return (
-    <Container>
-      <Row>
-        <h1>Home</h1>
-        {randomClimbCards}
-        {randomItemCards}
+    <Container fluid className='m-0'>
+      <h1>Home</h1>
+      <Row >
+        <Col xs={{span:2}}>
+          <h3>Climbs</h3>
+          {randomClimbCards}
+        </Col>
+        <Col xs={{span: 2, offset: 8}}>
+          <h3>Shop now</h3>
+          {randomItemCards}
+        </Col>
       </Row>
     </Container>
   )
